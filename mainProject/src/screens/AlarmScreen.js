2750be4cbe0972d4835555ba54492194ac7e7807
sircoon4 <sircoon4@gmail.com>
2020-08-03 07:34:00 +0900

    add datetimepicker

--- mainProject/src/screens/AlarmScreen.js
+++ mainProject/src/screens/AlarmScreen.js
@@ -1,33 +1,55 @@
 import React, {useContext, useState} from 'react';
-import { View, Text, Image, ScrollView, TextInput } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, Button, Platform } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AuthContext } from '../navigation/AuthProvider';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 const Stack = createStackNavigator();
 
 function AlarmMain() {
+    const [date, setDate] = useState(new Date(1598051730000));
+    const [mode, setMode] = useState('date');
+    const [show, setShow] = useState(false);
+
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShow(Platform.OS === 'ios');
+        setDate(currentDate);
+    };
+
+    const showMode = currentMode => {
+        setShow(true);
+        setMode(currentMode);
+    };
+
+    const showDatepicker = () => {
+        showMode('date');
+    };
+
+    const showTimepicker = () => {
+        showMode('time');
+    };
+
     return (
-        <ScrollView>
-            <Text>Some text</Text>
+        <View>
             <View>
-            <Text>Some more text</Text>
-            <Image
-            source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-            }}
-            style={{ width: 200, height: 200 }}
-            />
+                <Button onPress={showDatepicker} title="Show date picker!" />
             </View>
-            <TextInput
-            style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1
-            }}
-            defaultValue="You can type in me"
-            />
-        </ScrollView>
+            <View>
+                <Button onPress={showTimepicker} title="Show time picker!" />
+            </View>
+            {show && (
+                <DateTimePicker
+                    testID="dateTimePicker"
+                    value={date}
+                    mode={mode}
+                    is24Hour={true}
+                    display="default"
+                    onChange={onChange}
+                />
+            )}
+        </View>
     );
 }
 
