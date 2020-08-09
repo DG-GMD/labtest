b7763c440e2de5b102e1877542b02187db0a2df8
sircoon4 <sircoon4@gmail.com>
2020-08-10 02:31:33 +0900

    add alarm set page

--- mainProject/src/screens/AlarmScreen.js
+++ mainProject/src/screens/AlarmScreen.js
@@ -1,21 +1,52 @@
 import React, {useContext, useState} from 'react';
-import { View, Text, Image, ScrollView, TextInput, Button, Platform } from 'react-native';
+import { View, Text, Image, ScrollView, TextInput, Button, Platform, StyleSheet, TouchableOpacity } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AuthContext } from '../navigation/AuthProvider';
 import { alarmModule } from '../utils/jvmodules';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import RNDateTimePicker from '@react-native-community/datetimepicker';
 
 import LogoutButton from '../components/Logout';
 
 const Stack = createStackNavigator();
 
+export default function Alarm({navigation}){
+    const { user, logout } = useContext(AuthContext); 
+
+    //navigation.setOptions({ title: 'D-00 안녕하세요 000님' });
+
+    return (  
+    <Stack.Navigator>
+        <Stack.Screen name="AlarmMain" component={AlarmMain} />
+        <Stack.Screen name="AlarmSet" component={AlarmSet} />
+    </Stack.Navigator>
+    );
+}
+
 function AlarmMain({navigation}) {
+    navigation.setOptions({ headerTitle: props => <LogoutButton /> });
+
+    return (
+        <View>
+            <View>
+                <TouchableOpacity
+                    style = {styles.buttonContainer}
+                    onPress={() => navigation.navigate('AlarmSet')}
+                >
+                    <Text>설정</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    );
+};
+
+function AlarmSet({navigation}) {
+    navigation.setOptions({ headerTitle: props => <LogoutButton /> });
+
     const [date, setDate] = useState(new Date());
     const [mode, setMode] = useState('time');
     const [show, setShow] = useState(false);
 
-    //navigation.setOptions({ headerTitle: props => <LogoutButton /> });
     const onChange = (event, selectedDate) => {
         setShow(false);
 
@@ -45,60 +76,43 @@ function AlarmMain({navigation}) {
     return (
         <View>
             <View>
-                <Button onPress={showTimepicker} title = "알람시간 선택" />
-            </View>
-            <View>
-                <Button onPress={saveAlarm} title = "저장" />
-            </View>
-            {show && (
-                <DateTimePicker
+                <RNDateTimePicker
                     testID="dateTimePicker"
                     value={date}
-                    mode={mode}
+                    mode="time"
                     is24Hour={true}
-                    display="default"
+                    display="spinner"
                     onChange={onChange}
                 />
-            )}
+            </View>
+            <View>
+                <TouchableOpacity
+                    style = {styles.buttonContainer}
+                    onPress={() => navigation.navigate('AlarmMain')}
+                >
+                    <Text>취소</Text>
+                </TouchableOpacity>
+                <TouchableOpacity
+                    style = {styles.buttonContainer}
+                    onPress={() => saveAlarm()}
+                >
+                    <Text>저장</Text>
+                </TouchableOpacity>
+            </View>
         </View>
     );
-};
-
-function AlarmSet({navigation}) {
-    navigation.setOptions({ headerTitle: props => <LogoutButton /> });
-    return (
-        <ScrollView>
-            <Text>Some text</Text>
-            <View>
-            <Text>Some more text</Text>
-            <Image
-            source={{
-            uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
-            }}
-            style={{ width: 200, height: 200 }}
-            />
-            </View>
-            <TextInput
-            style={{
-            height: 40,
-            borderColor: 'gray',
-            borderWidth: 1
-            }}
-            defaultValue="You can type in me"
-            />
-        </ScrollView>
-    );
 }
 
-export default function Alarm({navigation}){
-    const { user, logout } = useContext(AuthContext); 
-
-    //navigation.setOptions({ title: 'D-00 안녕하세요 000님' });
-
-    return (  
-    <Stack.Navigator>
-        <Stack.Screen name="AlarmMain" component={AlarmMain} />
-        <Stack.Screen name="AlarmSet" component={AlarmSet} />
-    </Stack.Navigator>
-    );
-}
+const styles = StyleSheet.create({
+    buttonContainer: {
+        marginTop: 10,
+        width: 200,
+        height: 60,
+        backgroundColor: 'lightgreen',
+        padding: 10,
+        margin: 20,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8
+    },
+});
