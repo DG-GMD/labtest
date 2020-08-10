33ff43eb7a65ca6ad9de3381b3b6f2ccc821d1d7
sircoon4 <sircoon4@gmail.com>
2020-08-11 04:07:27 +0900

    modify alarm

--- mainProject/src/screens/AlarmScreen.js
+++ mainProject/src/screens/AlarmScreen.js
@@ -5,7 +5,7 @@ import { NavigationContainer, CommonActions } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { AuthContext } from '../navigation/AuthProvider';
 import { alarmModule } from '../utils/jvmodules';
-import RNDateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from '@react-native-community/datetimepicker';
 import {ScrollPicker} from 'react-native-value-picker';
 import database from '@react-native-firebase/database';
 
@@ -13,6 +13,7 @@ import {HOUR_DATA, MIN_DATA} from '../utils/timeData';
 import LogoutButton from '../components/Logout';
 
 import PopScreen from './PopScreen';
+import { Row } from 'react-native-table-component';
 
 const Stack = createStackNavigator();
 const reference = database().ref('/users/1000/alarm');
@@ -71,10 +72,31 @@ function AlarmMain({navigation, route}) {
     })();
 
     return (
-        <View>
-            <View>
-                {flag && <Text>
+        <View
+            style={{
+                flex: 1,
+                alignItems: 'center',
+            }}
+        >
+            <View style={{marginTop: 20}}>
+                <Text style={{fontSize: 30}}>
+                    단어 학습시간 알람
+                </Text>
+            </View>
+            <View
+                style={{
+                    width: "80%",
+                    margin: 30,
+                    alignItems: 'center',
+                    borderWidth: 2,
+                    borderColor: "grey",
+                }}
+            >
+                {flag && <Text style={{fontSize: 35}}>
                     매일 {route.params !== undefined ? route.params.setHour : pickedHourValue} : {route.params !== undefined ? route.params.setMin : pickedMinValue}
+                </Text>}
+                {!flag && <Text style={{fontSize: 25}}>
+                    설정된 알람이 없습니다.
                 </Text>}
             </View>
             <View>
@@ -82,7 +104,7 @@ function AlarmMain({navigation, route}) {
                     style = {styles.buttonContainer}
                     onPress={() => navigation.navigate('AlarmSet')}
                 >
-                    <Text>설정</Text>
+                    <Text>알람 설정</Text>
                 </TouchableOpacity>
             </View>
         </View>
@@ -114,6 +136,9 @@ function AlarmSet({navigation}) {
         // console.log('---------------in useeffect');
     });
 
+    const [date, setDate] = useState(new Date());
+    const [show, setShow] = useState(false);
+
     const [pickedHourValue, setPickedHourValue] = useState(0);
     const [pickedMinValue, setPickedMinValue] = useState(0);
     const [flag, setFlag] = useState(false);
@@ -134,17 +159,21 @@ function AlarmSet({navigation}) {
 
                 let order = parseInt(alarmData.order);
 
+                let dt = new Date();
                 if(order < 1){
-                    const dt = new Date();
                     setPickedHourValue(dt.getUTCHours());
                     setPickedMinValue(dt.getUTCMinutes());
                 }
                 else{
                     setPickedHourValue(alarmData.setHour);
                     setPickedMinValue(alarmData.setMin);
+
+                    dt.setUTCHours(alarmData.setHour);
+                    dt.setUTCMinutes(alarmData.setMin);
                 }
 
                 setFlag(true);
+                setDate(dt);
             })
         }
     })();
@@ -193,25 +222,72 @@ function AlarmSet({navigation}) {
             });
     };
 
+    const onChange = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        
+        setShow(Platform.OS === 'ios');
+        setPickedHourValue(currentDate.getUTCHours());
+        setPickedMinValue(currentDate.getUTCMinutes());
+        setDate(currentDate);
+    };
+
     return (
-        <View>
-            <View style={styles.PickerContainer}>
-                <ScrollPicker
-                currentValue={pickedHourValue}
-                extraData={pickedHourValue}
-                list={HOUR_DATA}
-                onItemPress={setPickedHourValue}
-                />
+        <View
+            style={{
+                flex: 1,
+                alignItems: 'center',
+            }}
+        >
+            <View
+                style={{
+                    marginTop: 30,
+                    flex: 1,
+                    flexDirection: 'row',
+                }}
+            >
+                {/* <View style={styles.PickerContainer}>
+                    <ScrollPicker
+                        currentValue={pickedHourValue}
+                        list={HOUR_DATA}
+                        onItemPress={setPickedHourValue}
+                    />
+                </View>
+                <Text 
+                    style = {{
+                        alignSelf: 'center',
+                        fontSize: 40,
+                    }}
+                >
+                    :
+                </Text>
+                <View style={styles.PickerContainer}>
+                    <ScrollPicker
+                        currentValue={pickedMinValue}
+                        list={MIN_DATA}
+                        onItemPress={setPickedMinValue}
+                    />
+                </View> */}
+                <TouchableOpacity
+                    style={{
+                        alignSelf: 'center',
+                    }}
+                    onPress={()=>setShow(true)}
+                >
+                    <Text
+                        style = {{
+                            fontSize: 60,
+                        }}
+                    >
+                        {pickedHourValue} : {pickedMinValue}
+                    </Text>
+                </TouchableOpacity>
             </View>
-            <View style={styles.PickerContainer}>
-                <ScrollPicker
-                currentValue={pickedMinValue}
-                extraData={pickedMinValue}
-                list={MIN_DATA}
-                onItemPress={setPickedMinValue}
-                />
-            </View>
-            <View>
+            <View
+                style={{
+                    flex: 2,
+                    flexDirection: 'row',
+                }}
+            >
                 <TouchableOpacity
                     style = {styles.buttonContainer}
                     onPress={() => navigation.navigate('AlarmMain')}
@@ -225,6 +301,16 @@ function AlarmSet({navigation}) {
                     <Text>저장</Text>
                 </TouchableOpacity>
             </View>
+            {show && (
+                <DateTimePicker
+                testID="dateTimePicker"
+                value={date}
+                mode="time"
+                is24Hour={true}
+                display="spinner"
+                onChange={onChange}
+                />
+            )}
         </View>
     );
 }
@@ -232,8 +318,8 @@ function AlarmSet({navigation}) {
 const styles = StyleSheet.create({
     buttonContainer: {
         marginTop: 10,
-        width: 200,
-        height: 60,
+        width: 80,
+        height: 40,
         backgroundColor: 'lightgreen',
         padding: 10,
         margin: 20,
@@ -242,10 +328,10 @@ const styles = StyleSheet.create({
         borderRadius: 8
     },
     PickerContainer: {
-        height: 120,
         width: 100,
+        height: 170,
+        marginHorizontal: 20,
         alignItems: 'center',
-        marginTop: 50,
     },
 });
 
