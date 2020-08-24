100b8dc0fd911c54b077ce5a7a6b1bb9d9980cf1
naem1023 <relilau00@gmail.com>
2020-08-24 23:03:57 +0900

    make script to organize json

---
+++ makeUserJson.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import json
+from collections import OrderedDict
+
+json_data = OrderedDict()
+
+"""
+condition 구분 : 1000, 2000, 3000 번대
+condition 별로 20명
+테스트 계정이므로 name='testUser', birth='000000'으로 통일
+"""
+numberList = [1000, 2000, 3000]
+
+for bigNumber in numberList:
+    for smallNumber in range(0, 20):
+        testNumber = bigNumber + smallNumber
+        json_data[testNumber] = {'name':'testUser', 'birth':123456}
+
+# print(json.dumps(json_data, indent=4))
+
+with open('users.json', 'w') as make_file:
+    json.dump(json_data, make_file, indent=4)
