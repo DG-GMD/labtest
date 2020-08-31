df3a34ae94009b17a71b850ad7a42cc52e04806b
naem1023 <relilau00@gmail.com>
2020-08-31 20:10:31 +0900

    new: ios branch

--- mainProject/App.js
+++ mainProject/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Providers from './src/navigation';
+import { NativeModules } from 'react-native';
 
 export default function App() {
+  
+  
   return <Providers />;
 }
